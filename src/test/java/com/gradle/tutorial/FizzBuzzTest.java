0d99a8b77b405c88067ae17a657857fcc3cce3e1
elena.kozlova <elena.kozlova@jetbrains.com>
2021-12-09 22:23:56 +0100

    the default gradle version 6.3 > 7.1 and default JUnit versions 4 >5 changed. Made updates to code accordingly.

--- src/test/java/com/gradle/tutorial/FizzBuzzTest.java
+++ src/test/java/com/gradle/tutorial/FizzBuzzTest.java
@@ -1,36 +1,38 @@
 package com.gradle.tutorial;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class FizzBuzzTest {
   @Test
   public void FizzBuzzNormalNumbers() {
 
     FizzBuzzProcessor fb = new FizzBuzzProcessor();
-    Assert.assertEquals("1", FizzBuzzProcessor.convert(1));
-    Assert.assertEquals("2", FizzBuzzProcessor.convert(2));
+    Assertions.assertEquals("1", FizzBuzzProcessor.convert(1));
+    Assertions.assertEquals("2", FizzBuzzProcessor.convert(2));
   }
 
   @Test
   public void FizzBuzzThreeNumbers() {
 
     FizzBuzzProcessor fb = new FizzBuzzProcessor();
-    Assert.assertEquals("Fizz", FizzBuzzProcessor.convert(3));
+    Assertions.assertEquals("Fizz", FizzBuzzProcessor.convert(3));
   }
 
   @Test
   public void FizzBuzzFiveNumbers() {
 
     FizzBuzzProcessor fb = new FizzBuzzProcessor();
-    Assert.assertEquals("Buzz", FizzBuzzProcessor.convert(5));
+    Assertions.assertEquals("Buzz", FizzBuzzProcessor.convert(5));
   }
 
   @Test
   public void FizzBuzzThreeAndFiveNumbers() {
 
     FizzBuzzProcessor fb = new FizzBuzzProcessor();
-    Assert.assertEquals("Buzz", FizzBuzzProcessor.convert(5));
+    Assertions.assertEquals("Buzz", FizzBuzzProcessor.convert(5));
   }
 
 
